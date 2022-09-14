ae0c64dac61414b6ea4a07f96f838908122cdf7d
Ariel Martinez <arielstereo@msn.com>
2022-09-13 22:41:40 -0300

    Add email info in contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -20,6 +20,7 @@ export default async function handler(req, res) {
       subject: `Contact form submission from ${name}`,
       html: `<p>You have a new contact form submission</p><br>
       <p><strong>Name: </strong> ${name} </p><br>
+      <p><strong>Email: </strong> ${email} </p><br>
       <p><strong>Message: </strong> ${message} </p><br>`,
     });
   } catch (err) {
